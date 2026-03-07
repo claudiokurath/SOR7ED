84bb0da233d426781106897b4324f161617d8ebe
claudiokurath-ai <claudio.kurath@gmail.com>
2026-03-07 02:44:32 +0000

    Move files out of app_new and into root

--- app_new/clean.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-try {
-    fs.rmSync('node_modules', { recursive: true, force: true });
-    console.log('Successfully deleted node_modules');
-} catch (e) {
-    console.error("Failed to delete node_modules:", e);
-}
