f6a98a2266e604b5bbfd2ddeaf3b6945a9b7ffe1
claudiokurath <claudio.kurath@gmail.com>
2026-03-07 02:15:40 +0000

    Add files via upload

--- test_notion.js
+++ test_notion.js
@@ -1,23 +1,21 @@
-const NOTION_API_KEY = 'ntn_t3590408908aUz0vVi2pdJGWtgrNspZczTJJQWqdlTsgVQ';
-const TOOLS_DB_ID = '08ac767d313845ca91886ce45c379b99';
+const fetch = require('node-fetch');
+const NOTION_API_KEY = "ntn_t3590408908aUz0vVi2pdJGWtgrNspZczTJJQWqdlTsgVQ";
+const BLOG_DB_ID = "db668e4687ed455498357b8d11d2c714";
 
-async function test() {
-    try {
-        const response = await fetch(`https://api.notion.com/v1/databases/${TOOLS_DB_ID}/query`, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${NOTION_API_KEY}`,
-                'Notion-Version': '2022-06-28',
-                'Content-Type': 'application/json'
-            }
-        });
-
-        const data = await response.json();
-        const statuses = [...new Set(data.results.map(r => r.properties.Status?.status?.name))];
-        console.log('Unique Tool Statuses:', statuses);
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
+async function run() {
+    const response = await fetch(`https://api.notion.com/v1/databases/${BLOG_DB_ID}/query`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${NOTION_API_KEY}`,
+            'Notion-Version': '2022-06-28',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            sorts: [{ property: 'Publish Date', direction: 'descending' }]
+        })
+    });
+    console.log(response.status);
+    const data = await response.text();
+    console.log(data);
 }
-
-test();
+run();
